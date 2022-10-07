0d235340773a4da9cb4d7694e1d023bdb116425d
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-10-07 09:29:17 -0700

    ADD: links to blog post

---
+++ init_files_variables_and_expansions/What_happens_when_you_type_ls_*.c
@@ -0,0 +1,5 @@
+https://medium.com/@miguelog.alejandro/when-you-type-ls-c-in-your-terminal-this-happens-in-the-background-5e9e0c3698ac
+
+https://www.linkedin.com/posts/miguel-colmenares-4a390024b_when-you-type-ls-c-in-your-terminal-this-activity-6984185494582534144-lCQm?utm_source=share&utm_medium=member_desktop
+
+https://twitter.com/MiguelCP94/status/1578419868386430977
